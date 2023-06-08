5ec8859d00f2504ff83e73599a017a67f04d1ee9
Lazar Stojanović <lakisd98@gmail.com>
2023-06-08 20:37:40 +0200

    Add: TypeScript function void & never

--- functions/app.ts
+++ functions/app.ts
@@ -1,18 +1,51 @@
-function square(num: number) {
+function square(num: number): number {
 	return num * num;
 }
 
-function greet(person: string) {
+function greet(person: string): string {
 	return `Hi there, ${person}`;
 }
 
-const doSomething = (person: string, number: number, isFunny: boolean) => {
-	if (isFunny) {
-		return `${person} is funny times ${number}`;
+const doSomething = (person: string, number: number, isFunny: boolean): string => {
+	if (!isFunny) {
+		throw new Error("Not Funny");
 	}
+	return `${person} is funny times ${number}`;
 };
 
 // Default prop
 function greetStranger(person: string = "Stranger") {
 	return `Hi there, ${person}`;
+}
+
+// TypeScript is smart to figure out what it will return
+function rand(num: number) {
+	if (Math.random() < 0.5) {
+		return num.toString();
+	}
+	return num;
+}
+
+// Anonymous function. TypeScript is smart to infer the type
+const colors = ["red", "orange", "yellow"];
+colors.map((color) => {
+	return color.toUpperCase();
+});
+
+// Void return nothing
+function printTwice(msg: string): void {
+	console.log(msg);
+	console.log(msg);
+}
+
+// Never type is function that never stop executing
+function makeError(msg: string): never {
+	// Infer type is void, but we can say it's never
+	throw new Error(msg);
+}
+
+function gameLoop(): never {
+	while (true) {
+		console.log("GAME LOOP RUNNING!");
+	}
 }
