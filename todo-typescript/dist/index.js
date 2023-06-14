08a3feba0c85bf8481e1664a1e4347aaf2bc0ed9
Lazar Stojanović <lakisd98@gmail.com>
2023-06-14 15:47:10 +0200

    Add: TypeScript localStorage todos

--- todo-typescript/dist/index.js
+++ todo-typescript/dist/index.js
@@ -4,20 +4,37 @@ const btn = document.getElementById("btn");
 const input = document.querySelector("#todo-input");
 const form = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
+const todoArray = readTodos();
+todoArray.forEach(createTodo);
 // btn.addEventListener("click", () => console.log("hello"));
 // Type assertion
 // let mystery: unknown = "Hello World!!!";
 // Doesn't change the type just threat it like a string
 // const numChars = (mystery as string).length;
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON == null) {
+        return [];
+    }
+    return JSON.parse(todosJSON);
+}
 function handleSubmit(event) {
     event.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todoArray.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todoArray));
+    input.value = "";
+}
+function createTodo(todo) {
     const newLi = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     newLi.append(checkbox);
     todoList.append(newLi);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
--- todo-typescript/src/index.ts
+++ todo-typescript/src/index.ts
@@ -4,6 +4,14 @@ const input = document.querySelector("#todo-input") as HTMLInputElement;
 const form = document.querySelector("#todo-form") as HTMLFormElement;
 const todoList = document.querySelector("#todo-list") as HTMLUListElement;
 
+interface Todo {
+	text: string;
+	completed: boolean;
+}
+
+const todoArray: Todo[] = readTodos();
+todoArray.forEach(createTodo);
+
 // btn.addEventListener("click", () => console.log("hello"));
 
 // Type assertion
@@ -12,18 +20,36 @@ const todoList = document.querySelector("#todo-list") as HTMLUListElement;
 // Doesn't change the type just threat it like a string
 // const numChars = (mystery as string).length;
 
+function readTodos(): Todo[] {
+	const todosJSON = localStorage.getItem("todos");
+	if (todosJSON == null) {
+		return [];
+	}
+	return JSON.parse(todosJSON);
+}
+
 function handleSubmit(event: SubmitEvent) {
 	event.preventDefault();
-	const newTodoText = input.value;
+
+	const newTodo: Todo = {
+		text: input.value,
+		completed: false,
+	};
+	createTodo(newTodo);
+	todoArray.push(newTodo);
+
+	localStorage.setItem("todos", JSON.stringify(todoArray));
+	input.value = "";
+}
+
+function createTodo(todo: Todo) {
 	const newLi = document.createElement("li");
 	const checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
 
-	newLi.append(newTodoText);
+	newLi.append(todo.text);
 	newLi.append(checkbox);
 	todoList.append(newLi);
-
-	input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
