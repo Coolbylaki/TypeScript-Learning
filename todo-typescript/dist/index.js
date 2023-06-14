3ca03adef6af7007b6c6c112b00208dc1d2668f5
Lazar Stojanović <lakisd98@gmail.com>
2023-06-14 14:59:26 +0200

    Add: TypeScript non-null assertion

---
+++ todo-typescript/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// With ! we guarantee that btn exist
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => console.log("hello"));
---
+++ todo-typescript/src/index.ts
@@ -0,0 +1,4 @@
+// With ! we guarantee that btn exist
+const btn = document.getElementById("btn")!;
+
+btn.addEventListener("click", () => console.log("hello"));
