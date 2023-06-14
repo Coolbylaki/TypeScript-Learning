f4e63c98e5374f0eb3e284a94f271b826ecf5284
Lazar Stojanović <lakisd98@gmail.com>
2023-06-14 15:23:33 +0200

    Add: TypeScript event type

--- todo-typescript/dist/index.js
+++ todo-typescript/dist/index.js
@@ -1,4 +1,23 @@
 "use strict";
 // With ! we guarantee that btn exist
 const btn = document.getElementById("btn");
-btn.addEventListener("click", () => console.log("hello"));
+const input = document.querySelector("#todo-input");
+const form = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
+// btn.addEventListener("click", () => console.log("hello"));
+// Type assertion
+// let mystery: unknown = "Hello World!!!";
+// Doesn't change the type just threat it like a string
+// const numChars = (mystery as string).length;
+function handleSubmit(event) {
+    event.preventDefault();
+    const newTodoText = input.value;
+    const newLi = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    todoList.append(newLi);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
--- todo-typescript/src/index.ts
+++ todo-typescript/src/index.ts
@@ -1,4 +1,29 @@
 // With ! we guarantee that btn exist
 const btn = document.getElementById("btn")!;
+const input = document.querySelector("#todo-input") as HTMLInputElement;
+const form = document.querySelector("#todo-form") as HTMLFormElement;
+const todoList = document.querySelector("#todo-list") as HTMLUListElement;
 
-btn.addEventListener("click", () => console.log("hello"));
+// btn.addEventListener("click", () => console.log("hello"));
+
+// Type assertion
+// let mystery: unknown = "Hello World!!!";
+
+// Doesn't change the type just threat it like a string
+// const numChars = (mystery as string).length;
+
+function handleSubmit(event: SubmitEvent) {
+	event.preventDefault();
+	const newTodoText = input.value;
+	const newLi = document.createElement("li");
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+
+	newLi.append(newTodoText);
+	newLi.append(checkbox);
+	todoList.append(newLi);
+
+	input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
