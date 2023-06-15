0c1d064821dc12d0b9ffc28b8446407c5413fe33
Lazar Stojanović <lakisd98@gmail.com>
2023-06-15 17:00:02 +0200

    Add: TypeScript generics examples

--- generics/dist/index.js
+++ generics/dist/index.js
@@ -5,3 +5,31 @@ const colors = ["red", "blue"];
 const inputEl = document.querySelector("#username");
 const buttonEl = document.querySelector(".btn");
 buttonEl.addEventListener("click", () => console.log("hey"));
+// Classic example of why we would use generics
+function numIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+// Works, but defeats purpose of TS
+function identity(item) {
+    return item;
+}
+// Generic
+function genericIdentity(item) {
+    return item;
+}
+genericIdentity("Hello");
+genericIdentity(34);
+genericIdentity(true);
+genericIdentity({ name: "John", breed: "Russian blue" });
+// Another example
+function getRandomElement(numArray) {
+    const maxIndex = numArray.length - 1;
+    const randomIndex = Math.floor(Math.random() * (maxIndex + 1));
+    return numArray[randomIndex];
+}
--- generics/src/index.ts
+++ generics/src/index.ts
@@ -6,3 +6,44 @@ const inputEl = document.querySelector<HTMLInputElement>("#username")!;
 const buttonEl = document.querySelector<HTMLButtonElement>(".btn")!;
 
 buttonEl.addEventListener("click", () => console.log("hey"));
+
+// Classic example of why we would use generics
+function numIdentity(item: number): number {
+	return item;
+}
+
+function stringIdentity(item: string): string {
+	return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+	return item;
+}
+
+// Works, but defeats purpose of TS
+function identity(item: any): any {
+	return item;
+}
+
+// Generic
+function genericIdentity<T>(item: T): T {
+	return item;
+}
+
+genericIdentity<string>("Hello");
+genericIdentity<number>(34);
+genericIdentity<boolean>(true);
+
+interface Cat {
+	name: string;
+	breed: string;
+}
+
+genericIdentity<Cat>({ name: "John", breed: "Russian blue" });
+
+// Another example
+function getRandomElement<T>(numArray: T[]): T {
+	const maxIndex = numArray.length - 1;
+	const randomIndex = Math.floor(Math.random() * (maxIndex + 1));
+	return numArray[randomIndex];
+}
