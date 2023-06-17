91085da9b4c6b034f048e1238d98dcf6f09dc30a
Lazar Stojanović <lakisd98@gmail.com>
2023-06-18 00:49:56 +0200

    Add: TypeScript useState

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,18 +1,20 @@
 import ShoppingList from "./components/ShoppingList";
+import Item from "./models/item";
+import { useState } from "react";
 
 function App() {
-	const items = [
-		{
-			id: 1,
-			product: "Lemon",
-			quantity: 3,
-		},
-		{
-			id: 2,
-			product: "Chicken breast",
-			quantity: 2,
-		},
-	];
+	const [items, setItems] = useState<Item[]>([]);
+
+	// {
+	//   id: 1,
+	//   product: "Lemon",
+	//   quantity: 3,
+	// },
+	// {
+	//   id: 2,
+	//   product: "Chicken breast",
+	//   quantity: 2,
+	// },
 
 	return (
 		<>
